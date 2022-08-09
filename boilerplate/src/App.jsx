03260c813605110b8bf859cadcfc009c8896a20f
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-08-09 15:03:22 +0500

    Migrate boilerplate to vite - react-scripts comes with a lot of bloat - react-scripts comes with eslint which was clashing with the root eslint install - Add eslint rule to account for vite as a devdependency

--- boilerplate/src/App.jsx
+++ boilerplate/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-export default function App() {
+function App() {
   return (
     <div>
-      <h1>Hello!</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello World!</h1>
     </div>
   );
 }
+
+export default App;
