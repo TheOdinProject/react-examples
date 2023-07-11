8449d2b7bd95ab9ffe9030fe13cbc0c37c8a1fbb
Francis Eugene Casibu <projectnevz@gmail.com>
2023-07-11 15:07:58 +0800

    add memoization lesson example

---
+++ memoization-lesson-example/src/App.jsx
@@ -0,0 +1,84 @@
+import React, {
+  memo, Profiler, useMemo, useState, useRef,
+} from 'react';
+
+// Lets wrap the ButtonComponent with a memo
+// eslint-disable-next-line react/function-component-definition, react/prop-types
+const ButtonComponent = memo(({ children, onClick }) => {
+  // To simulate a very slow render
+  let i = 0;
+  let j = 0;
+  // If you have a really good machine, increase the iteration count to experience it better
+  const ITERATION_COUNT = 10_000;
+  while (i < ITERATION_COUNT) {
+    while (j < ITERATION_COUNT) {
+      j += 1;
+    }
+    i += 1;
+    j = 0;
+  }
+
+  return (
+    <button type="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+});
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const durationRef = useRef({ baseDuration: 0, actualDuration: 0 });
+
+  const handleClick = () => {
+    setCount((prevState) => prevState + 1);
+  };
+
+  const memoizedHandleClick = useMemo(() => handleClick, []);
+
+  const onRender = (_id, _phase, actualDuration, baseDuration) => {
+    durationRef.current = { actualDuration, baseDuration };
+  };
+
+  return (
+    <div>
+      <Profiler id="buttoncomponent" onRender={onRender}>
+        <h1>{count}</h1>
+        {/* Swap memoizedHandleClick with handleClick and vice versa */}
+        <ButtonComponent onClick={memoizedHandleClick}>
+          Click me!
+        </ButtonComponent>
+      </Profiler>
+      <div>
+        <h2>Base Duration:</h2>
+        <p>
+          <strong>{durationRef.current.baseDuration}</strong>
+          <span>
+            {' '}
+            - The number of milliseconds estimating how much time it would take
+            to re-render the entire Profile subtree without any optimizations.
+            It is calculated by summing up the most recent render durations of
+            each component in the tree. This value estimates a worst-case cost
+            of rendering (e.g. the initial mount or a tree with no memoization).
+            Compare actualDuration against it to see if memoization is working.
+          </span>
+        </p>
+        <h2>Actual Duration:</h2>
+        <p>
+          <strong>{durationRef.current.actualDuration}</strong>
+          <span>
+            {' '}
+            - The time spent rendering the Profiler component and its descendant
+            for the current update, 0 means it rendered extremely fast! This
+            will be the same as the Base Duration on the first render
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return <Counter />;
+}
+
+export default App;
