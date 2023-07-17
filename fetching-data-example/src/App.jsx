2c276a523b7b3de8bbcec6d6ffdb03fb0e7ef73c
Kevin Browne <kbrowne97@gmail.com>
2023-07-17 10:17:09 -0400

    adds fetching-data-example for fetching data with react lesson

---
+++ fetching-data-example/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Profile from './Profile';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Profile delay={1000} />;
+    </div>
+  );
+}
+
+export default App;
