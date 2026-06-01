fe95c0b4d9acbc515236ac3eb5a3ebe486a3017b
mao-sz <122839503+mao-sz@users.noreply.github.com>
2026-06-01 21:53:21 +0100

    Use top-level shared vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // where `npm run X` was executed i.e. the dir for the example being run
+  root: process.env.INIT_CWD,
   plugins: [react()],
 });
