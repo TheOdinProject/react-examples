8b83ef655ebf925812a3564d7074cc086f4c0257
mao-sz <122839503+mao-sz@users.noreply.github.com>
2026-06-02 08:46:47 +0100

    Define Vite root in CLI command

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // where `npm run X` was executed i.e. the dir for the example being run
-  root: process.env.INIT_CWD,
   plugins: [react()],
 });
